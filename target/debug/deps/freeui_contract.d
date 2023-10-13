db5a3b55700478dd047539de08516e88d998cbdb
Alexander Tran <pichstudent2004@gmail.com>
2023-10-14 00:31:59 +0700

    first commit

---
+++ target/debug/deps/freeui_contract.d
@@ -0,0 +1,5 @@
+D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\target\debug\deps\freeui_contract.exe: src\main.rs
+
+D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\target\debug\deps\freeui_contract.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/freeui-contract.d
@@ -0,0 +1,1 @@
+D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\target\debug\freeui-contract.exe: D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\src\main.rs
