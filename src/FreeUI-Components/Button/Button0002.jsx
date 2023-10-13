9f465a7e6709782be0b7068679f2eb5305b6d086
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-13 20:23:40 +0700

    FirstCommit

---
+++ src/FreeUI-Components/Button/Button0002.jsx
@@ -0,0 +1,93 @@
+const Button0002 = styled.button`
+  height: 3em;
+  width: 8em;
+  border: none;
+  border-radius: 10em;
+  background: #016dd9;
+  font-size: 17px;
+  color: #ffffff;
+  font-family: inherit;
+  font-weight: 500;
+
+  &:hover {
+    animation-name: shakeAnimation;
+    animation-duration: 0.3s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+
+  @keyframes shakeAnimation {
+    0% {
+      transform: translate(0);
+    }
+    20% {
+      transform: translate(-2px, 2px);
+    }
+    40% {
+      transform: translate(-2px, -2px);
+    }
+    60% {
+      transform: translate(2px, 2px);
+    }
+    80% {
+      transform: translate(2px, -2px);
+    }
+    100% {
+      transform: translate(0);
+    }
+  }
+`;
+
+const props = {
+  copyBtn:
+    "const Button0002 = styled.button`\n" +
+    `height: 3em;
+  width: 8em;
+  border: none;
+  border-radius: 10em;
+  background: #016dd9;
+  font-size: 17px;
+  color: #ffffff;
+  font-family: inherit;
+  font-weight: 500;
+
+  &:hover {
+    animation-name: shakeAnimation;
+    animation-duration: 0.3s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+
+  @keyframes shakeAnimation {
+    0% {
+      transform: translate(0);
+    }
+    20% {
+      transform: translate(-2px, 2px);
+    }
+    40% {
+      transform: translate(-2px, -2px);
+    }
+    60% {
+      transform: translate(2px, 2px);
+    }
+    80% {
+      transform: translate(2px, -2px);
+    }
+    100% {
+      transform: translate(0);
+    }
+  }` +
+    "`;" +
+    `\n return(
+    <Button0002>Button 0002</Button0002>
+)`,
+  component: <Button0002>Button0002</Button0002>,
+};
+return (
+  <>
+    <Widget src="freeui.testnet/widget/CardMain.CardMini" props={props} />
+  </>
+);
