ef5f485454c7211ce677daa4768edc950384bac5
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-13 23:39:54 +0700

    u

--- src/FreeUI-Components/Button/Button0020.jsx
+++ src/FreeUI-Components/Button/Button0020.jsx
@@ -111,7 +111,7 @@ const props = {
     width: 100%;
     height: 100%;
   }
-uuuuuuuuuuuuuuuuuuuuuuuuuuuu
+uuuuuuuuuuuuuuuuuuuuuuuuuuuuu
   &:active::after {
     transition: 0s;
     transform: translate(0, 5%);
