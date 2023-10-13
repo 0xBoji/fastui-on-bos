fc8eac1cf8a7b00dfdabae642b80f91029ccae5c
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-13 22:03:51 +0700

    update

---
+++ src/FreeUI-Components/Button/Button0016.jsx
@@ -0,0 +1,73 @@
+const Button0016 = styled.button`
+  width: 150px;
+  height: 60px;
+  border: 3px solid #315cfd;
+  border-radius: 45px;
+  transition: all 0.3s;
+  cursor: pointer;
+  background: white;
+  font-size: 1.2em;
+  font-weight: 550;
+  font-family: 'Montserrat', sans-serif;
+
+  &:hover {
+    background: #315cfd;
+    color: white;
+    font-size: 1.5em;
+  }
+`;
+
+let copyThis =
+  props.copyThis ||
+  "const Button0016 = styled.button`\n" +
+    `  width: 150px;
+  height: 60px;
+  border: 3px solid #315cfd;
+  border-radius: 45px;
+  transition: all 0.3s;
+  cursor: pointer;
+  background: white;
+  font-size: 1.2em;
+  font-weight: 550;
+  font-family: 'Montserrat', sans-serif;
+
+  &:hover {
+    background: #315cfd;
+    color: white;
+    font-size: 1.5em;
+  }` +
+    "`;" +
+    `\n return(
+        <Button0016>Button 0016</Button0016>
+        )`;
+
+const props = {
+  copyBtn:
+      "const Button0016 = styled.button`\n" +
+    `  width: 150px;
+  height: 60px;
+  border: 3px solid #315cfd;
+  border-radius: 45px;
+  transition: all 0.3s;
+  cursor: pointer;
+  background: white;
+  font-size: 1.2em;
+  font-weight: 550;
+  font-family: 'Montserrat', sans-serif;
+
+  &:hover {
+    background: #315cfd;
+    color: white;
+    font-size: 1.5em;
+  }` +
+    "`;" +
+    `\n return(
+        <Button0016>Button 0016</Button0016>
+        )`,
+  component:           <Button0016>Button 0016</Button0016>
+};
+return (
+  <>
+    <Widget src="freeui.testnet/widget/CardMain.CardMini" props={props} />
+  </>
+);
