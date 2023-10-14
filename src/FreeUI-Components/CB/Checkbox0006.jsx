9bedeb65d27ff64b48e4e5dffe9534e55c3c6fa0
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 22:14:04 +0700

    update

--- src/FreeUI-Components/CB/Checkbox0006.jsx
+++ src/FreeUI-Components/CB/Checkbox0006.jsx
@@ -71,7 +71,7 @@ const props = {
   input {
     display: none;
   }
-  uupdatupdateepupdateupdupdateatedate
+  uupdupdateatupdateepupdateupdupdateatedate
     span {
     display: block;
     position: absolute;
