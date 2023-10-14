9e8482ad4368275594ad7e32429785b4f19307c1
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 18:45:16 +0700

    update

---
+++ src/FreeUI-Components/CB/Checkbox0004.jsx
@@ -0,0 +1,109 @@
+const Checkbox = styled.input`
+  position: relative;
+  width: 20px;
+  height: 20px;
+  border-radius: 2px;
+  background-color: #bbb;
+  transition: all 0.3s;
+
+  &:checked {
+    animation: rotateAnimation 0.3s ease-in forwards;
+    background-color: rgb(120, 190, 120);
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    border: solid #fff;
+    display: block;
+    width: 0.3em;
+    height: 0.6em;
+    border-width: 0 0.2em 0.2em 0;
+    z-index: 1;
+    opacity: 0;
+    right: calc(50% - 0.3em);
+    top: calc(50% - 0.6em);
+    transform: rotate(0deg);
+    transition: all 0.3s;
+    transform-origin: center center;
+  }
+
+
+  @keyframes rotateAnimation {
+    0% {
+      opacity: 1;
+      transform: scale(1) rotateY(0deg);
+    }
+
+    50% {
+      opacity: 0;
+      transform: scale(0.8) rotateY(180deg);
+    }
+
+    100% {
+      opacity: 1;
+      transform: scale(1) rotateY(360deg);
+    }
+  }
+`;
+
+const props = {
+  copyBtn:
+    "const Checkbox = styled.input`\n" +
+    `  position: relative;
+  width: 20px;
+  height: 20px;
+  border-radius: 2px;
+  background-color: #bbb;
+  transition: all 0.3s;
+
+  &:checked {
+    animation: rotateAnimation 0.3s ease-in forwards;
+    background-color: rgb(120, 190, 120);
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    border: solid #fff;
+    display: block;
+    width: 0.3em;
+    height: 0.6em;
+    border-width: 0 0.2em 0.2em 0;
+    z-index: 1;
+    opacity: 0;
+    right: calc(50% - 0.3em);
+    top: calc(50% - 0.6em);
+    transform: rotate(0deg);
+    transition: all 0.3s;
+    transform-origin: center center;
+  }
+
+
+  @keyframes rotateAnimation {
+    0% {
+      opacity: 1;
+      transform: scale(1) rotateY(0deg);
+    }
+
+    50% {
+      opacity: 0;
+      transform: scale(0.8) rotateY(180deg);
+    }
+
+    100% {
+      opacity: 1;
+      transform: scale(1) rotateY(360deg);
+    }
+  }` +
+    "\n`;" +
+    `\n return(
+    <Checkbox type="checkbox" />
+  )`,
+  component: <Checkbox type="checkbox" />,
+};
+return (
+  <>
+    <Widget src="freeui.testnet/widget/CardMain.CardMini" props={props} />
+  </>
+);
