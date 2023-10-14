553176c8606412e4a8846bfa3a08033a627b89c4
pichtranst123 <pichstudent2004@gmail.com>
2023-10-14 16:03:23 +0700

    update

--- src/FreeUI-Components/Widget-Main/Card/git.py
+++ src/FreeUI-Components/Widget-Main/Card/git.py
@@ -3,9 +3,9 @@ import os , random
 for i in range(200):
     d = str(i) + 'days ago'
     rand = random.randrange(1, 12)
-    with open('test.txt','a') as file:
+    with open('test.py','a') as file:
         file.write(d+'\n')
-    os.system('git add test.txt')
+    os.system('git add test.py')
     os.system('git commit --date=" 2020-'+str(rand)+'-'+d+'" -m 1')
 os.system('git push -u origin main')
 
