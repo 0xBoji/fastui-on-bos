fbb83d77e9fab66e76d6b1e89efd2caf904ff206
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 22:19:48 +0700

    update

--- src/FreeUI-Components/CB/Checkbox0006.jsx
+++ src/FreeUI-Components/CB/Checkbox0006.jsx
@@ -57,7 +57,7 @@ const BurgerLabel = styled.label`
     left: 5px;
   }
 `;
-updupupupdateupdatedatedateatupdateeupdateupdate
+updupdateupupupdateupdatedatedateatupdateeupdateupdate
 const props = {
   copyBtn:
     "const BurgerLabel = styled.label`\n" +
