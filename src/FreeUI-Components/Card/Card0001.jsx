3564b59af74a6fcfe1d327dadddf57c529bbd466
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 14:29:40 +0700

    update

--- src/FreeUI-Components/Card/Card0001.jsx
+++ src/FreeUI-Components/Card/Card0001.jsx
@@ -158,7 +158,7 @@ const props = {
             <Action type="button">Get started</Action>
           </Footer>
         </CardMain>
-  )`,updateupdateupdateupdateupdateupdateupdateupdateupdate
+  )`,updateupdateupdateupdateupdateupdateupdateupdateupdateupdate
   component: (
     <CardMain>
       <Header></Header>
