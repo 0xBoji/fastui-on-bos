73f49c3de3ac82bd1b68cc82a2f6b6426a196500
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 21:31:20 +0700

    update

--- src/FreeUI-Components/Widget-Main/Card/CardLarge.jsx
+++ src/FreeUI-Components/Widget-Main/Card/CardLarge.jsx
@@ -315,7 +315,7 @@ return (
         overlay={
           <Tooltip>{state.copied ? "Copied!" : "Copy to clipboard"}</Tooltip>
         }
-      >uupupdaupdupdupdateateupdaupdateteatetupdateupdateedaupdatetupdateepdateupdatupdateeupdateupdate
+      >update
         <TextCopy
           onClick={() => {
             clipboard.writeText(copyBtn).then(() => {
