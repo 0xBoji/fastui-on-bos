544ae520b278a171502897967fed714ed0166063
Alexander Tran <126134422+pichtranst123@users.noreply.github.com>
2023-10-14 22:09:33 +0700

    update

--- src/FreeUI-Components/CB/Checkbox0006.jsx
+++ src/FreeUI-Components/CB/Checkbox0006.jsx
@@ -70,7 +70,7 @@ const props = {
 
   input {
     display: none;
-  }updateupdaupdateteuuuuupdatepdatepdatepdaupdateteuuupdatepdupupdatedateaupdatetepupupdatedatedatepdate
+  }
 
   span {
     display: block;
