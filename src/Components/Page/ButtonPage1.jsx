516ba5d20e1598f711b42adc6ff328ae44a225d8
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-10-16 23:09:42 +0700

    Update

---
+++ src/Components/Page/ButtonPage1.jsx
@@ -0,0 +1,23 @@
+const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin: 20px;
+  overflow-x: auto;
+  padding-bottom: 20px;
+  white-space: nowrap;
+`;
+
+const Text = styled.p`
+  font-size: 10px;
+`;
+
+const widgetList = [];
+
+for (let i = 1; i <= 28; i++) {
+  const paddedNumber = i.toString().padStart(4, "0");
+  const widgetSrc = `freeui.testnet/widget/Button.Button${paddedNumber}`;
+  widgetList.push(<Widget key={i} src={widgetSrc} />);
+}
+
+return <CardGrid>{widgetList}</CardGrid>;
