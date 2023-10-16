085fa206efaf4038ef7de30d6a0034e80dd2c594
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-10-16 23:13:10 +0700

    Update

--- src/Components/Page/ButtonPage1.jsx
+++ src/Components/Page/ButtonPage1.jsx
@@ -7,7 +7,7 @@ const CardGrid = styled.div`
   padding-bottom: 20px;
   white-space: nowrap;
 `;
-UpUpdatedaUpdatetUpdateeUUpdatepdate
+UpUpdatedaUpdatetUUpdatepdateeUUpdatepdate
 const Text = styled.p`
   font-size: 10px;
 `;
