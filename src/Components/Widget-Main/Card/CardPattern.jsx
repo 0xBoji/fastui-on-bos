d65f3c3f2f06af4be768f35443daee097d24f9f4
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-10-18 00:32:47 +0700

    update-code

--- src/Components/Widget-Main/Card/CardPattern.jsx
+++ src/Components/Widget-Main/Card/CardPattern.jsx
@@ -124,7 +124,7 @@ return (
         overlay={
           <Tooltip>{state.copied ? "Copied!" : "Copy to clipboard"}</Tooltip>
         }
-      >update-codeupdate-code
+      >update-codeupdate-codeupdate-code
         <TextCopy
           onClick={() => {
             clipboard.writeText(copyBtn).then(() => {
