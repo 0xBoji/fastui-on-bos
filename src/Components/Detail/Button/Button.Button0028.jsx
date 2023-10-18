781e3b856dd2e944ef0e902d1a63b86a80f205d3
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-10-18 23:34:17 +0700

    Update detail

--- src/Components/Detail/Button/Button.Button0028.jsx
+++ src/Components/Detail/Button/Button.Button0028.jsx
@@ -134,4 +134,4 @@ return (
     <Widget src="freeui.testnet/widget/CardMain.CardDetail" props={props} />
   </>
 );
-Update detailUpdate detailUpdate detaUpdate detailil
+Update detaiUpdate detaillUpdate detailUpdate detaUpdate detailil
