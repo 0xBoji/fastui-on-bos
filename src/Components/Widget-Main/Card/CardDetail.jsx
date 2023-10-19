30b77d5b23f7173775671f4a24f4f96e8b70a06a
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-10-19 23:53:41 +0700

    Update detail

--- src/Components/Widget-Main/Card/CardDetail.jsx
+++ src/Components/Widget-Main/Card/CardDetail.jsx
@@ -230,8 +230,7 @@ const SvgIconCopy = styled.span`
     fill: white;
   }
 `;
-const EditorContainer = styled.div`
-
+Update detail
 const EditorContainer = styled.div`
   top:20px;
   background-color: #1e1e1e;
