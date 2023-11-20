f4236ad039f85ddc058c1df233e665f6c203683e
Trần Ngọc Hoàng Pích <126134422+pichtranst123@users.noreply.github.com>
2023-11-21 02:33:47 +0700

    update contract

--- src/componentsbyUser.rs
+++ src/componentsbyUser.rs
@@ -1,41 +1,46 @@
-use near_sdk::{
-    collections::{LazyOption, LookupMap, UnorderedSet},
-    near_bindgen,
-    serde::{Deserialize, Serialize},
-    AccountId, CryptoHash, PanicOnDefault,
-  };
-  
-  use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-  use schemars::JsonSchema;
-  
-  use super::{
-    space::{SpaceId, SpaceMetadata},
-    thread::{ThreadId, ThreadMetadata},
-    user::{JsonUser, UserId},
-  };
-  
-  /// The `ThreadScoreContractMetadata` struct represents metadata for an ThreadScore contract.
-  #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, JsonSchema)]
-  #[serde(crate = "near_sdk::serde")]
-  pub struct TitleWidgetBOS {
-  
-    /// Title of the Widget contract.
-    pub title: String,
-  
-    pub symbol: String,
+use near_sdk::{AccountId, Balance, Promise, WasmQuery};
+
+#[derive(BorshDeserialize, BorshSerialize)]
+struct Message {
+  id: String,
+  text: String,
+}
+
+#[near_contract]
+pub struct FastuiSCTest {
+  pub messages: Vec<Message>,
+}
+
+impl FastuiSCTest {
+  pub fn new(owner: AccountId) -> Self {
+    Self {
+      messages: vec![],
+    }
   }
-  
-  /// The `ThreadScoreContract` struct represents an ThreadScore Contract in the system.
-  #[near_bindgen]
-  #[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
-  pub struct saveComponents {
-    pub owner_id: AccountId,
+
+  pub fn add_message(&mut self, message: Message) -> Promise {
+    self.messages.push(message);
+    self.save_messages()
   }
-  
-  #[derive(BorshSerialize)]
-  pub enum StorageKey {
-    saveComponents,
-    TitleWidgetBOS,
+
+  pub fn get_messages(&self) -> Vec<Message> {
+    self.messages.clone()
   }
-  
-  
+
+  pub fn delete_message(&mut self, message_id: String) -> Promise {
+    let index = self
+      .messages
+      .iter()
+      .position(|message| message.id == message_id)
+      .unwrap();
+    self.messages.remove(index);
+    self.save_messages()
+  }
+
+  fn save_messages(&self) -> Promise {
+    let encoded_messages = near_sdk::serde_json::to_string(&self.messages).unwrap();
+    self.env().storage().set("messages", encoded_messages).then(|_| {
+      Promise::new(self.env().current_account_id())
+    })
+  }
+}
--- target/debug/freeui-contract.d
+++
@@ -1,1 +0,0 @@
-D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\target\debug\freeui-contract.exe: D:\Web3\RUST\FreeUI-contract\Freeui\freeui-contract\src\main.rs
